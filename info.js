fb77b0cd01f3cf00bdd459fdc1ce74479cf5b180
Dilip Kumar Yadav <yadavdip32@gmail.com>
2024-10-19 23:55:18 +0530

    using only function calling in forEach loop

--- info.js
+++ info.js
@@ -1,15 +1,12 @@
 const arr =['red','blue','pink','green'];
-
-// var len = arr.length;
-
 // for(var i=0;arr[i]!=undefined;i++){
 //     console.log(arr[i]);
 // }
 var sum =0;
- arr.forEach(dipu = ((raju)=>{
+ arr.forEach(function(raju){
      console.log(raju);
-     sum++;
- }))
- console.log(sum);
+
+
+ })
 
 
