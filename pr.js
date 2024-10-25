03c4d95d6ea6d8d64e87122178120550e623d901
Dilip Kumar Yadav <yadavdip32@gmail.com>
2024-10-25 11:18:46 +0530

    reduce and filter

--- pr.js
+++ pr.js
@@ -1,5 +1,36 @@
-const arr =[1,5,9,7,1,0];
+const arr =[
+    {
+        id:1,
+        age:20
+    },
+    {
+        id:2,
+        age:30
+    },
+    {
+        id:3,
+        age:26
+    },
+]
+// var sum=0;
 
-for(let i=0;i<arr.length;i++){
-}
-console.log(arr[1]);
+// arr.filter((index)=>{
+//     if(index.age>20) {
+//         sum=sum+index.age;
+//         console.log(index.age);}
+//     else{console.log("zero");}
+
+// })
+
+// console.log(sum);
+
+
+const sum=arr.reduce((prev,cur)=>{
+    if(cur.age>20){
+    return prev+cur.age;
+    }
+    return prev;
+},0);
+
+console.log(sum);
+
